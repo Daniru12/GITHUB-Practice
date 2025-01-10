a65349c3719294042f8f997352098a4f4dffde39
Daniru-Punsith <punsithdaniru@gmail.com>
2025-01-10 23:30:58 +0530

    change app.js and delete sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,37 +2,24 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 // Import Pages
 import KFCHeader from "./Components/header";
 import KFCFooter from "./Components/footer";
 import Home from "./Pages/Home";
 import Signup from "./Pages/signup";
 
-function AppContent() {
-  return (
-    <div className="App">
-        <main className="flex-grow">
-        <header>
-          <KFCHeader />
-        </header>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/signup" element={<Signup/>}/>
-          </Routes>
-          <footer>
-            <KFCFooter/>
-          </footer>
-        </main>
-    </div>
-  );
-}
-
 function App() {
   return (
-    <Router>
-      <AppContent />
-    </Router>
+    <div className="App">
+      <Router>
+        <KFCHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <KFCFooter />
+      </Router>
+    </div>
   );
 }
 
